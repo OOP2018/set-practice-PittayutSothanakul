a173782dc25e0074214df5f4f4b53d81bf92fc2d
Pittayut Sothanakul <pittayut.sot@ku.th>
2018-02-08 11:16:39 +0700

    push the code

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -6,66 +6,85 @@ import java.util.*;
 public class SetPractice {
 	/**
 	 * Add zero or more items to the set, and print what is being added.
+	 * 
 	 * @parameter set is the Set to add items to
 	 * @parameter values zero or more Strings to add to set
 	 */
-	public static void addAll(Set<String> set, String ...values) {
-		// add all the values to the set. 
+	public static void addAll(Set<String> set, String... values) {
+		// add all the values to the set.
 		// print each one as you add it
+		System.out.println("Adding elements");
+		for (String arg : values) {
+			set.add(arg);
+			System.out.println("  " + arg);
+		}
 	}
-	
+
 	/**
 	 * Print all the elements in a set, using an Iterator.
 	 */
 	public static void print(Set<String> set) {
 		// create an Iterator using set.iterator()
 		// then use the iterator to print everything in the set
+		Iterator<String> iter = set.iterator();
+		while (iter.hasNext()) {
+			String next = iter.next();
+			System.out.print(" " + next);
+
+		}
+		System.out.println();
 	}
-	
+
 	/**
 	 * Explore behavior of a Set.
 	 */
-	public static void setExplorer( ) {
+	public static void setExplorer() {
 		// 1. Create a HashSet and add strings: dog, ant, bird, elephant, cat
-		
-		
+		Set<String> set = new HashSet<String>();
+		addAll(set, "dog", "ant", "bird", "elephant", "cat","cat");
+
 		// 2. Print elements in the set using an Iterator over the Set.
-		// You're going to need to do this again, so write a method print(Set set) to do it.
-		
-		
-		// 3. are the elements printed in same order than you added them? 
+		// You're going to need to do this again, so write a method print(Set
+		// set) to do it.
+		print(set);
+		// 3. are the elements printed in same order than you added them?
 		// What does this tell you about Sets?
-		
-		
-		// 4. Remove all the elements from the set, using a method that 
+		System.out.println("Question: Are the elements printed in same order you added them?");
+		// 4. Remove all the elements from the set, using a method that
 		// defined in Set (and Collection). What is the method to use?
-		
-		
+		System.out.println("Remove the elements");
+		set.clear();
 		// 5. Add same elements as exercise 1, but in a different order,
 		// such as: "cat", "elephant", "bird", "ant", "dog"
-		
-		
-		// 6. Print the elements in the set.  Is the order same as before?
-		
-		
-		// 7. Add duplicate elements to the set.  Add another "cat" and "dog".
-		// To ensure that these are *distinct* objects use 'set.add(new String("cat"))'
+		addAll(set, "cat", "elephant", "bird", "ant", "dog");
+		// 6. Print the elements in the set. Is the order same as before?
+		print(set);
+		// 7. Add duplicate elements to the set. Add another "cat" and "dog".
+		// To ensure that these are *distinct* objects use 'set.add(new
+		// String("cat"))'
 		// and new String("dog").
 		System.out.println("Add duplicate items to the set ");
-		
-		
-		// 8. Print the elements in set.  Are there any duplicates?
+
+		// 8. Print the elements in set. Are there any duplicates?
 		// What property of Set does this demonstrate?
 		System.out.println("Set contains: ");
-		
-		
+
 		// 9. Repeat the exercises using a TreeSet instead of HashSet.
-		//  What is different when you use TreeSet?
-		
-		// Hint: Use Polymorphism.  Make the Set object be a *parameter* to the
-		// method so you can easily use a different set just by changing the parameter!
-		
+		// What is different when you use TreeSet?
+		Set<String> set2 = new TreeSet<String>();
+		addAll(set2, "dog", "ant", "bird", "elephant", "cat");
+		print(set2);
+		System.out.println("Question: Are the elements printed in same order you added them?");
+		System.out.println("Remove the elements");
+		set2.clear();
+		addAll(set2, "cat", "elephant", "bird", "ant", "dog");
+		print(set2);
+		// Hint: Use Polymorphism. Make the Set object be a *parameter* to the
+		// method so you can easily use a different set just by changing the
+		// parameter!
+
 		// 10. Repeat again using TreeSet with a Comparator.
+		Set<String> set3 = new TreeSet<String>();
 	}
 
 	public static void main(String[] args) {
